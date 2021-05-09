f06a767093cc2427d0f5924ce595d690fc9134d1
Pablo Corso <pabcorso@gmail.com>
2021-05-09 13:34:37 +0200

    Remove legacy tests

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,16 @@
 const { printResult } = require("./utils/print");
-const levelsRankAverage = require("./levelsRankAverage");
 const levelsRankDouble = require("./levelsRankDouble");
-const { prToCentiseconds } = require("./utils/pr");
+const { recordToCentiseconds } = require("./utils/record");
 
 const levelsData = [
   {
     name: "Lvl 1",
     prs: ["3:50", "3:60", "3:90", "3:90", "3:90", "60:59:59"].map(
-      prToCentiseconds
+      recordToCentiseconds
     ),
   },
-  { name: "Lvl 2", prs: ["10:00", "10:50", "11:00"].map(prToCentiseconds) },
-  { name: "Lvl 3", prs: ["21:00"].map(prToCentiseconds) },
+  { name: "Lvl 2", prs: ["10:00", "10:50", "11:00"].map(recordToCentiseconds) },
+  { name: "Lvl 3", prs: ["21:00"].map(recordToCentiseconds) },
 ];
 
-printResult("Rank average", levelsRankAverage(levelsData));
 printResult("Rank double", levelsRankDouble(levelsData));
-// console.log(html.prettyPrint(htmlResult, { indent_size: 2 }));
